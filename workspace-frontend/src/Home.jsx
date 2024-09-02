bedcea9b4b92e36bbf72530c23b128c1f3f98907
Kirtan Jain <kirtanjain0504@gmail.com>
2024-09-02 05:44:52 +0530

    Added getBookings

--- workspace-frontend/src/Home.jsx
+++ workspace-frontend/src/Home.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import { VenueSelect } from './venues/VenueSelect';
 import { ClockWithBookings } from './ClocksWithBookings';
-
-const mockBookings = [
-  {
-    start: "4:30",
-    end: "6:00",
-    name: "NJACK",
-    reason: "Coding Workshop"
-  },
-  {
-    start: "8:00",
-    end: "10:00",
-    name: "SYAHI",
-    reason: "Creative Writing Meetup"
-  },
-  {
-    start: "2:00",
-    end: "3:30",
-    name: "ART CLUB",
-    reason: "Painting Session"
-  }
-];
+import { BookingButton } from './components/BookingButton';
+import { DateClock } from './components/DateClock';
+import { mockBookings } from './venues/constants';
 
 export const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <VenueSelect /> {/* Use the VenueSelect component */}
+      <DateClock/>
       <ClockWithBookings bookings={mockBookings} />
       <ClockWithBookings bookings={mockBookings} />
+      
+      <BookingButton/>
     </div>
   );
 };
--- workspace-frontend/src/venues/constants.js
+++ workspace-frontend/src/venues/constants.js
@@ -7,4 +7,23 @@ export const Venues = {
     nssRooom: "NSS Room",
     hoscaRoom: "HOSCA ROOM (SAC)"
   };
-  
+  export const mockBookings = [
+    {
+      start: "4:30",
+      end: "6:00",
+      name: "NJACK",
+      reason: "Coding Workshop"
+    },
+    {
+      start: "8:00",
+      end: "10:00",
+      name: "SYAHI",
+      reason: "Creative Writing Meetup"
+    },
+    {
+      start: "2:00",
+      end: "3:30",
+      name: "ART CLUB",
+      reason: "Painting Session"
+    }
+  ];
